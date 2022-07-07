f3a937200807732992e25d20cc8e73e450521fb0
Daniel Eldon <eldondaniel2@gmail.com>
2022-07-08 02:28:13 +0300

    creating tiles

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
+  const object = props.info;
+  const values = Object.values(object);
+
+  const info = values.map((value, index) => {
+    let className;
+    if (index === 0) {
+      className='tile-title';
+    } else {
+      className='tile'
+    }
+    return <p key={index} className={className} >{value}</p>
+    
+  });
+
   return (
     <div className="tile-container">
-      
+      {info}
     </div>
   );
 };
