45673606d6a04c7f4d7e2282791743a5d8136cb3
Daniel Eldon <eldondaniel2@gmail.com>
2022-07-08 02:52:23 +0300

    Adding Appointments

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,62 @@
-import React from "react";
+import {React, useState, useEffect}from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
+export const AppointmentsPage = (props) => {
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [alert, setAlert] = useState('');
+  const defaultListValue = 'No contact selected';
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
+    props.addAppointment(title, contact, date, time);
+    setTitle('');
+    setContact('');
+    setDate('');
+    setTime('');
+    document.getElementById('contactList').value = defaultListValue;
    
   };
+
+  useEffect(()=>{
+    for(const appointmentItem of props.appointments) {
+      if(appointmentItem.date === date && appointmentItem.time === time){
+        setAlert('An appointment has already been reserved for that time and date');
+        console.log('Appointment already reserved');
+      } else {
+        setAlert('');
+      }
+    }
+  }, [props.appointments, time, date])
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+        contacts={props.contacts}
+        title={title}
+        setTitle={setTitle}
+        date={date}
+        setDate={setDate}
+        time={time}
+        setTime={setTime}
+        contact={contact}
+        setContact={setContact}
+        handleSubmit={handleSubmit}
+        alert={alert}
+        defaultListValue={defaultListValue}
+
+
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList object={props.appointments}/>
       </section>
     </div>
   );
