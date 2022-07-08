132df4f320b6e1c8df7bbdbb2cdccdb91ce429be
Daniel Eldon <eldondaniel2@gmail.com>
2022-07-08 03:12:06 +0300

    Selecting contacts

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
+  const names = props.contacts.map((contact) => {
+    return <option value={contact.name} key={contact.name} >{contact.name}</option>
+  });
+  
   return (
-    ContactPicker
+    <form>
+    <label htmlFor="contactList">Choose contact:</label>
+    <select id="contactList" onChange={props.onChange} required >
+      <option value={props.defaultListValue} key="default" selected="selected" >{props.defaultListValue}</option>
+      {names}
+    </select>
+  </form>
   );
 };
